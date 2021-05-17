7f90ea66c9957d6612b503da5d45fa14f660c9ef
reinhardtc <reinhardtc22@gmail.com>
2021-05-17 12:49:41 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
@@ -9,3 +11,15 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'what is your name?',
+    },
+  ])
+  .then(answers => console.log(answers));
+
+console.log(inquirer);
